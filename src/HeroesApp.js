bfd4106310bbbd961d58d6538e938a67e4987310
David Granados <davidgranados73@gmail.com>
2021-06-05 10:48:44 -0500

    S14 C170: Inicio del Proyecto

---
+++ src/HeroesApp.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const HeroesApp = (props) => {
+  return <div></div>;
+};
+
+HeroesApp.propTypes = {};
+
+export default HeroesApp;
