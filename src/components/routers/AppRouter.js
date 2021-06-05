72bfdac133a5bbe706717f7f464c8c2d5b4a89f8
David Granados <davidgranados73@gmail.com>
2021-06-05 18:20:16 -0500

    S15 - C192: Rutas públicas

--- src/components/routers/AppRouter.js
+++ src/components/routers/AppRouter.js
@@ -1,17 +1,25 @@
-import React, {useContext} from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import LoginScreen from "../login/LoginScreen";
 import DashboardRoutes from "./DashboardRoutes";
-import {PrivateRoute} from "../../routers/PrivateRoute";
-import {AuthContext} from "../../auth/AuthContext";
+import { PrivateRoute } from "../../routers/PrivateRoute";
+import { AuthContext } from "../../auth/AuthContext";
+import { PublicRoute } from "../../routers/PublicRoute";
 
 const AppRouter = () => {
-  const {authState: {logged}} = useContext(AuthContext);
+  const {
+    authState: { logged },
+  } = useContext(AuthContext);
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path="/login" component={LoginScreen} />
+          <PublicRoute
+            exact
+            path="/login"
+            component={LoginScreen}
+            isAuthenticated={logged}
+          />
           <PrivateRoute
             path="/"
             component={DashboardRoutes}
