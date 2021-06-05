5b3c44bc46712af1ad48156d7230d8d6700f9777
David Granados <davidgranados73@gmail.com>
2021-06-05 17:24:08 -0500

    S15 - C188: Context y Reducer del app

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useReducer } from "react";
 import AppRouter from "./components/routers/AppRouter";
+import { AuthContext } from "./auth/AuthContext";
+import { authReducer } from "./auth/authReducer";
 
-const HeroesApp = (props) => {
-  return <AppRouter />;
+const init = () => {
+  return JSON.parse(localStorage.getItem("user")) || { logged: false };
+};
+
+const HeroesApp = () => {
+  const [authState, dispatchState] = useReducer(authReducer, {}, init);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        authState,
+        dispatchState,
+      }}
+    >
+      <AppRouter />
+    </AuthContext.Provider>
+  );
 };
 
 export default HeroesApp;
